6c89e360b11276e3d54fe1a57d7f1d9b59d6ee7a
Carter Bertolini <carterbertolini@gmail.com>
2023-11-04 17:05:21 -0400

    Basic popup component.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 pub mod app;
+pub mod popup;
+
 use cfg_if::cfg_if;
 
 cfg_if! {
---
+++ src/popup.rs
@@ -0,0 +1,22 @@
+use leptos::*;
+use leptos::html::Dialog;
+
+#[component]
+pub fn Popup(children: Children) -> impl IntoView {
+    let dialog_ref = create_node_ref::<Dialog>();
+
+    let on_close = move |_| {
+        let dialog = dialog_ref.get().expect("dialog_ref should be loaded");
+
+        dialog.set_open(false);
+    };
+
+    view! {
+        <dialog _ref=dialog_ref open>
+            <button autofocus on:click=on_close>Close</button>
+            {
+                children().nodes.into_iter().collect::<Vec<_>>()
+            }
+        </dialog>
+    }
+}
