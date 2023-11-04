c06b5dfc287f176b3b528f44158dd830247035f3
AidanWestphal <93747131+AidanWestphal@users.noreply.github.com>
2023-11-04 18:09:11 -0400

    Comment with Star Ratng

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,13 @@
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
+use crate::comment::Comment;
 
 #[component]
 pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
-
+ 
     view! {
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod app;
+pub mod comment;
 use cfg_if::cfg_if;
 
 cfg_if! {
