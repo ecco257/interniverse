863916135ac79b1e65340157763f120cf6ecd0e2
ecco257 <72117210+ecco257@users.noreply.github.com>
2023-11-05 04:04:53 -0500

    Edit listing previews to be responsive to search input

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,13 @@
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
+use leptos::leptos_dom::Text;
 use crate::popup::PopupPage;
 use crate::header::Header;
 use crate::search_bar::SearchBar;
 use crate::listing_prev::ListingPrev;
 use crate::login::LoginPage;
+use leptos::leptos_dom::logging::console_log;
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -34,22 +36,72 @@ pub fn App() -> impl IntoView {
     }
 }
 
+#[derive(Clone, Debug, PartialEq)]
+pub struct Listing {
+    pub company_name: String,
+    pub position: String,
+    pub num_comments: String,
+}
+
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
 
+	let (all_listings, set_all_listings) = create_signal(vec![
+        Listing {
+            company_name: "Company 1".to_string(),
+            position: "Position 1".to_string(),
+            num_comments: "1".to_string(),
+        },
+        Listing {
+            company_name: "Company 2".to_string(),
+            position: "Position 2".to_string(),
+            num_comments: "2".to_string(),
+        },
+        Listing {
+            company_name: "Company 3".to_string(),
+            position: "Position 3".to_string(),
+            num_comments: "3".to_string(),
+        },
+    ]);
+
+	let (search_query, set_search_query) = create_signal(String::new());
+
+    let (filtered_listings, set_filtered_listings) = create_signal::<View>(View::Text(Text::new("No results".to_string().into())));
+
     view! {
 		<div class="home-page">
-			<Header/>
-			<SearchBar/>
-			<div class="listings">
-				<ListingPrev company_name="Company 1".to_string() position="Position 1".to_string() num_comments="1".to_string()/>
-				<ListingPrev company_name="Company 2".to_string() position="Position 2".to_string() num_comments="2".to_string()/>
-				<ListingPrev company_name="Company 3".to_string() position="Position 3".to_string() num_comments="3".to_string()/>
-			</div>
-		</div>
+            <Header/>
+            <SearchBar 
+                search_query=search_query 
+                set_search_query=set_search_query
+            />
+            <div>{filtered_listings}</div>
+            {
+                create_effect(move |_| {
+                    let filter_text = search_query.get().to_lowercase();
+                        let all_listings = all_listings.get();
+						let filtered = all_listings.iter()
+							.filter(|listing| listing.company_name.to_lowercase().contains(&filter_text) ||
+                                listing.position.to_lowercase().contains(&filter_text))
+							.collect::<Vec<&Listing>>();						
+						let content = filtered.iter().map(|listing| {
+							view! {
+								<ListingPrev
+									company_name=listing.company_name.clone()
+									position=listing.position.clone()
+									num_comments=listing.num_comments.clone()
+								/>
+							}
+                        }).collect_view();
+
+                        set_filtered_listings(content);
+                });
+            }
+        </div>
     }
 }
+
 
 /// Renders the new post page of your application.
 /// This is where users can create new posts.
--- src/search_bar.rs
+++ src/search_bar.rs
@@ -1,8 +1,10 @@
 use leptos::*;
 
 #[component]
-pub fn SearchBar() -> impl IntoView {
-    let (search_query, set_search_query) = create_signal(String::new());
+pub fn SearchBar(
+	search_query: ReadSignal<String>, 
+	set_search_query: WriteSignal<String>,
+) -> impl IntoView {
 
     view! {
         <div class="search-bar-wrapper">
@@ -10,8 +12,10 @@ pub fn SearchBar() -> impl IntoView {
                 type="text"
                 class="search-bar"
                 placeholder="Search for a company or position..."
-                value={search_query}
-                on:input=move |event| { set_search_query.update(|search_query| *search_query = event.to_string().into()); }
+                on:input=move |event| {
+					set_search_query(event_target_value(&event));
+				}
+				prop:value=search_query
             />
         </div>
     }
